7c4843bdf654bba9fcc38b27209519363490424a
Troy Davidson <clandaith@gmail.com>
2016-05-17 19:37:03 -0600

    updated code

--- src/main/resources/db/migration/V1__init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE PERSON (
-	id serial not null primary key,
-	first_name varchar(255) not null,
-	last_name varchar(255) not null
-);
-
-insert into PERSON (first_name, last_name) values ('Dave', 'Syer');
