d555acd810ccd6cfad40f4622768702d9e3a4ed4
Troy Davidson <clandaith@gmail.com>
2016-05-25 22:35:37 -0600

    Added fail test

--- src/test/java/com/clandaith/gum/Blork.java
+++ src/test/java/com/clandaith/gum/Blork.java
@@ -1,11 +1,11 @@
 package com.clandaith.gum;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 
-import com.clandaith.gum.SpringBootWebApplication;
 import com.clandaith.gum.controllers.TestController;
 import com.clandaith.gum.entities.Foobar;
 
@@ -14,7 +14,12 @@ import com.clandaith.gum.entities.Foobar;
 @SpringApplicationConfiguration(SpringBootWebApplication.class)
 public class Blork {
 
-	private TestController testController = new TestController();
+	private final TestController testController = new TestController();
+
+	@Test
+	public void willFail() {
+		Assert.fail();
+	}
 
 	@Test
 	public void blah() {
