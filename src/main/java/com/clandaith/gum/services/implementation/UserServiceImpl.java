7ba70ec26bf20638605ec44b3607b98538dffe48
Troy Davidson <clandaith@gmail.com>
2016-05-31 19:31:59 -0600

    made to work

--- src/main/java/com/clandaith/gum/services/implementation/UserServiceImpl.java
+++ src/main/java/com/clandaith/gum/services/implementation/UserServiceImpl.java
@@ -3,13 +3,16 @@ package com.clandaith.gum.services.implementation;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.clandaith.gum.entities.User;
 import com.clandaith.gum.repositories.UserRepository;
 import com.clandaith.gum.services.UserService;
 import com.google.common.collect.Lists;
 
-public class UserSystemImpl implements UserService {
+@Service
+public class UserServiceImpl implements UserService {
 	private UserRepository userRepository;
 
 	@Autowired
@@ -27,12 +30,13 @@ public class UserSystemImpl implements UserService {
 		return userRepository.findOne(id);
 	}
 
-	@Override
-	public User getUserByUsername(String username) {
-		return null;
-	}
+	// @Override
+	// public User getUserByUsername(String username) {
+	// return null;
+	// }
 
 	@Override
+	@Transactional
 	public User saveUser(User user) {
 		return userRepository.save(user);
 	}
